dfcc53114a5adb47c05a59fc9772ec78b81ab618
Max de Vocht <maxdevocht@hotmail.com>
2024-04-08 14:31:30 +0200

    Added Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className="logo" src="./src/assets/img/logo.png" alt="Logo" />
+            <a href="#" className="nav--menu"><i class="ri-menu-4-line"></i></a>
+        </nav>
+    )
+}
