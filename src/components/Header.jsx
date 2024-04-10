1ceea36ec6ac03d83b5cf5c270490f274433043b
Max de Vocht <maxdevocht@hotmail.com>
2024-04-10 14:47:57 +0200

    changed nav/header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header">
-            <video autoPlay loop muted className="header--vid" src="./src/assets/img/banner.mp4"></video>
+            <img autoPlay loop muted className="header--vid" src="./src/assets/img/banner-2.gif"></img>
         </header>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { EqualFill } from 'akar-icons';
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <div className="nav--menu">
-                <a href="#" className="menu--btn"><i class="ri-menu-line"></i></a>
-            </div>
+            <img src="./src/assets/img/logo.png" alt="Logo" className="logo" />
+            <a href="#" className="menu--btn"><EqualFill strokeWidth={2} size={36} /></a>
         </nav>
     )
 }
