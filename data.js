8b947e2bb88e323fdd3bef255e0680cb26e43f84
Max de Vocht <maxdevocht@hotmail.com>
2024-04-18 14:12:05 +0200

    Added data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        id: 1,
+        img: "./src/assets/img/nft2.png",
+        link: "METASEED",
+        job: "Artist"
+    },
+    {
+        id: 1,
+        img: "./src/assets/img/nft3.png",
+        link: "MONOLITH",
+        job: "Developer"
+    },
+]
