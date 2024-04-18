0155b663fe7a86ce69043f77aae65e212f6cb9e4
Max de Vocht <maxdevocht@hotmail.com>
2024-04-18 13:55:52 +0200

    Added decoder component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Coder from './components/Coder'
+import Decoder from './components/Decoder'
 import './App.css'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <Header />
       <Coder />
+      <Decoder />
     </>
   )
 }
---
+++ src/components/Decoder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Decoder() {
+    return (
+        <section className="decoder">
+            <h1 className="title black">DECODER</h1>
+            <p className="description2">Own a <span>sssymbolsss</span> NFT? Use the <span>DECODER</span> to find out which letters are contained in your <span>sssymbolsss</span>.<br /> These letters can be used in the <span>CODER</span> above.</p>
+            <button className="btn">DECODER</button>
+        </section>
+    )
+}
