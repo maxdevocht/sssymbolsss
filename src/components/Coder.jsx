31bd92156f4bc754495d09ed78d7907a8a05ae91
Max de Vocht <maxdevocht@hotmail.com>
2024-05-13 10:18:36 +0200

    Update
    Added infinity scroller

--- src/components/Coder.jsx
+++ src/components/Coder.jsx
@@ -3,7 +3,25 @@ import React from "react";
 export default function Coder() {
     return (
         <section className="coder">
-            <h1 className="title blue">CODER</h1>
+            {/* <h1 className="title blue">CODER</h1> */}
+            <div className="coder__list">
+                <div className="wrapper1">
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                    <h1 className="title blue">CODER</h1>
+                </div>
+            </div>
             <p className="description">Own a <span>sssymbolsss</span> NFT? Use the <span>CODER</span> to create your own codes.<br /> 
             Right-click-save the code and sent to frens!</p>
             <button className="btn">CODER</button>
--- src/components/Decoder.jsx
+++ src/components/Decoder.jsx
@@ -3,7 +3,24 @@ import React from "react";
 export default function Decoder() {
     return (
         <section className="decoder">
-            <h1 className="title black">DECODER</h1>
+            <div className="coder__list">
+                <div className="wrapper2">
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                    <h1 className="title black">DECODER</h1>
+                </div>
+            </div>
             <p className="description2">Own a <span>sssymbolsss</span> NFT? Use the <span>DECODER</span> to find out which letters are contained in your <span>sssymbolsss</span>.<br /> These letters can be used in the <span>CODER</span> above.</p>
             <button className="btn">DECODER</button>
         </section>
